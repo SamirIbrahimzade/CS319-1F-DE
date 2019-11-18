9911a2f63ac07f6eaf3e0bb6271cc2ded49b110f
qazad <ayberkgorgun@hotmail.com>
2019-11-18 22:35:31 +0300

    bullet image and move

--- src/Model/Bullet.java
+++ src/Model/Bullet.java
@@ -1,15 +1,33 @@
 package Model;
 
+import javafx.scene.image.Image;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Bullet extends GameObject {
     private boolean isEnemyBullet;
     private final int speed = 50;
 
     public Bullet(){
-
+        try (FileInputStream inputStream = new FileInputStream("MediaFiles/bullet.png")) {
+            img = new Image(inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void move(){
+    public void move(int direction){
 
+        if(direction == 0){
+            this.x += speed;
+        }
+        if(direction == 1){
+            this.x -= speed;
+        }
     }
 
     public void setEnemyBullet(boolean enemyBullet) {
