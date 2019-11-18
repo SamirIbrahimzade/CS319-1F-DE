3286bc951f0ce6aa7222f6db012a66a3ec60429b
SamirIbrahimzade <samir.ibrahimzade@ug.bilkent.edu.tr>
2019-11-18 13:58:28 +0400

    - MediaFiles added

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -14,6 +14,37 @@ public class Player extends GameObject {
     }
     public void move(int direction){
 
+        //if direction = 0 move UP
+        //if direction = 1 move DOWN
+        //if direction = 2 move LEFT
+        //if direction = 3 move RIGHT
+
+        //curDirection 0 for Left
+        //curDirection 1 for Right
+
+        if(direction == 0){
+            this.y += 10;
+        }
+        if(direction == 1){
+            this.y -= 10;
+        }
+        if(direction == 2){
+            if(this.curDirection == 1){
+                //change png file
+                //this.img =
+            }
+                this.x -= 10;
+
+        }
+        if(direction == 3){
+            if(this.curDirection == 0){
+                //change png file
+                //this.img =
+            }
+
+            this.x += 10;
+        }
+
     }
 
     public void setLives(int lives) {
