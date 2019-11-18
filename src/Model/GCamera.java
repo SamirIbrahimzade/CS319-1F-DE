7142b5fd22f48891832478d046b18a312e04f72e
SamirIbrahimzade <samir.ibrahimzade@ug.bilkent.edu.tr>
2019-11-18 15:35:39 +0400

    - KeyListener added - GCamera updated

--- src/Model/GCamera.java
+++ src/Model/GCamera.java
@@ -1,13 +1,32 @@
 package Model;
 
+import Controller.Game;
+import com.sun.management.GcInfo;
+
 public class GCamera extends GameObject {
-    private float position;
+    private float x,y;
 
-    public float getPosition() {
-        return position;
+    public GCamera(float x, float y){
+        this.x = x;
+        this.y = y;
     }
 
-    public void setPosition(float position) {
-        this.position = position;
+    public void tick(Player player){
+        x = player.getX() - 500;
+
     }
+
+    public void setX(float x){
+        this.x = x;
+    }
+    public void setY(float y){
+        this.y = y;
+    }
+    public float getX(float x){
+        return x;
+    }
+    public float getY(float y){
+        return y;
+    }
+
 }
